01612f92c749e69f894d4118e65af2d1ea411936
Atapi <34801996+Sterophonick@users.noreply.github.com>
2025-04-21 09:38:30 -0600

    test

--- site/disclosure.php
+++ site/disclosure.php
@@ -43,7 +43,7 @@ echo constructPageHeader("Atapi's Homepage!");
             Caddy w/ PHP-FPM 8.3
         </li>
         <li>
-            Site git commit: <code><?php exec('git rev-parse HEAD', $output); print_r($output);?></code>
+            Site git commit: <code><?php shell_exec('git rev-parse HEAD');?></code>
         </li>
     </ul>
 </div>
