1fed0acc82cae2cdf66003e7de5b92a45c2c6a5e
Atapi <34801996+Sterophonick@users.noreply.github.com>
2025-04-21 19:36:47 -0600

    403

---
+++ 403.php
@@ -0,0 +1,19 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/util.php';
+echo constructPageHeader("Atapi's Homepage! :: HTTP 403");
+
+?>
+
+<h1><img style="vertical-align:middle" src="\assets\img\404\icon.png">HTTP 403</h1>
+<img src="/assets/img/404/403.jpg"><br />
+<br />
+<p>
+whatcha doin here? :3c <br/>
+</p>
+
+<?php
+
+echo constructPageFooter();
+
+?>
