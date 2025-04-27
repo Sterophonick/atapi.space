9af5b804eff8e7b5d5d46f13dfaa41c27ff6a54e
Atapi <34801996+Sterophonick@users.noreply.github.com>
2025-04-26 22:27:33 -0600

    projectx

--- includes/header.php
+++ includes/header.php
@@ -596,6 +596,7 @@ $musicPaths = array(
     "kfaraday - On the Road", "/assets/snd/music/on_the_road.mp3",
     "Nintendo - Main Theme (Nintendo Video)", "/assets/snd/music/nintendo_video.mp3",
     "Naoki Kodaka - Underground Concourse (PocketNES Dendy Mix)", "/assets/snd/music/pocketnes_concourse.mp3",
+    "Allister Brimble - px.bladswede remix!", "/assets/snd/music/projectx.mp3",
 );
 
 ?>
