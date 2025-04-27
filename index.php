4a8f6aca402e649ac191d5c3c1ba62cd903355f1
Atapi <34801996+Sterophonick@users.noreply.github.com>
2025-04-27 10:06:53 -0600

    lol

--- index.php
+++ index.php
@@ -95,6 +95,8 @@
                     <a href="https://web0.halotroop.com/"><img src="/assets/img/buttons/halotroop.gif"></a>
 
                     <a href="https://flurrys.neocities.org/"><img src="/assets/img/buttons/nescartridges.gif"></a>
+
+                    <a href="https://darksdomain.neocities.org/"><img src="/assets/img/buttons/darksdomain.gif"></a>
                     </marquee>
                     <br/>
                 </p>
