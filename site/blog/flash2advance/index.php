e105a69d6249325d7a6194c59d5c8105d29bc357
Atapi <34801996+Sterophonick@users.noreply.github.com>
2025-05-01 11:52:40 -0600

    flash2advance

--- site/blog/flash2advance/index.php
+++ site/blog/flash2advance/index.php
@@ -6,7 +6,7 @@ echo constructPageHeader("Atapi's Homepage! :: Blog :: Flash2Advance-ing your Ga
 ?>
 
 <h1><img style="vertical-align:middle" src="/assets/img/blog/icon.png"> Flash2Advance-ing your Game Boy Advance</h1>
-<p style="margin-top: -25px;">"A good and cool device for your GBA"<br/></p><br/>
+<p style="margin-top: -25px;">No, dad, I'm not pirating Nintendo games, I swear on my life.<br/></p><br/>
 <p>
 May ??, 2025<br/>
 Category: Infodump<br/>
@@ -14,9 +14,22 @@ Category: Infodump<br/>
 <br />
 <h2>Introduction</h2>
 <p>
-    The year is 2003. If you were playing handheld video games at this time, you undoubtedly owned a Nintendo Game Boy Advance. You're very happy with your choice of handheld, not that there was much at the time, playing games like Mario &amp; Luigi, Fire Emblem, Pokémon, and Sonic Advance, however you longed to be able to do more with it, whether it be watching videos or listening to music. The iPod was out at the time, but a new unit would run you a <i>spicy</i> $299 USD ($521.65 in 2025!), and your prayers of watching cartoons or movies would not be answered until 2004 with Game Boy Advance Video. You also happen to be a computer toucher who loves getting the most out of your devices at the cost of convenience. What's one to do in this predicament?<br/><br/>
+    The year is 2003. If you were playing handheld video games at this time, you undoubtedly owned a Nintendo Game Boy Advance. You're very happy with your choice of handheld, not that there was much at the time, playing games like Mario &amp; Luigi, Fire Emblem, Pokémon, and Sonic Advance, however you longed to be able to do more with it, whether it be watching F.R.I.E.N.D.S. or listening to your Linkin Park CDs. The iPod was out at the time, but a new unit would run you a <i>spicy</i> $299 USD ($521.65 in 2025!), and your prayers of watching cartoons or movies would not be answered until 2004 with Game Boy Advance Video. You also happen to be a computer toucher who loves getting the most out of your devices at the cost of convenience. What's one to do in this predicament?<br/><br/>
 
-    You purchase a flashcart of course!
+    You go onto Google, you look up "how 2 listen 2 music on my gameboy", and discover the wonderful world of Flashcarts.<br/><br/>
+
+    At the time, these cartridges were much more limited than what we have now, usually requiring a fancy schmancy linker that often connected to your PC over a parallel connection. Later flash linkers would connect over USB but these too were eventually dropped in favor of CompactFlash, and later on, SD cards.
+
+    basic outline:
+    - showing off my collection
+    - teardowns of cartridges
+    - PogoShell (& Plugins)
+    - Meteo
+    - GBA Movie Player
+    - Pirating games
+    - EZ-Writer
+    - Diehard buttons
+
 </p>
 <?php
 
