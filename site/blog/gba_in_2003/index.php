c90ec27180db40e066e195fca45a71b036f45a0c
Atapi <34801996+Sterophonick@users.noreply.github.com>
2025-04-30 23:18:34 -0600

    starfall

---
+++ site/blog/gba_in_2003/index.php
@@ -0,0 +1,25 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/util.php';
+echo constructPageHeader("Atapi's Homepage! :: Blog :: Flash2Advance-ing your Game Boy Advance");
+
+?>
+
+<h1><img style="vertical-align:middle" src="/assets/img/blog/icon.png"> Flash2Advance-ing your Game Boy Advance</h1>
+<p style="margin-top: -25px;">"A good and cool device for your GBA"<br/></p><br/>
+<p>
+May ??, 2025<br/>
+Category: Infodump<br/>
+</p>
+<br />
+<h2>Introduction</h2>
+<p>
+    The year is 2003. If you were playing handheld video games at this time, you undoubtedly owned a Nintendo Game Boy Advance. You're very happy with your choice of handheld, not that there was much at the time, playing games like Mario &amp; Luigi, Fire Emblem, Pokémon, and Sonic Advance, however you longed to be able to do more with it, whether it be watching videos or listening to music. The iPod was out at the time, but a new unit would run you a <i>spicy</i> $299 USD ($521.65 in 2025!), and your prayers of watching cartoons or movies would not be answered until 2004 with Game Boy Advance Video. You also happen to be a computer toucher who loves getting the most out of your devices at the cost of convenience. What's one to do in this predicament?<br/><br/>
+
+    You purchase a flashcart of course!
+</p>
+<?php
+
+echo constructPageFooter();
+
+?>
--- site/index.php
+++ site/index.php
@@ -189,6 +189,7 @@ src="/assets/img/buttons/siivagunner.png"></a>
 <a href="https://flipperzero.one/"><img src="/assets/img/buttons/flipper.gif"></a>
 <a href="https://flurrys.neocities.org/"><img src="/assets/img/buttons/nescartridges.gif"></a>
 <a href="https://darksdomain.neocities.org/"><img src="/assets/img/buttons/darksdomain.gif"></a>
+<a href="https://starfall.com/"><img src="/assets/img/buttons/starfall.png"></a>
 <br />
 <img src="/assets/img/home/linuxfox-comp.gif">
 <marquee id="potat" style="display: none;"><img src="/assets/img/home/potato.gif"></marquee>
