72da8b83a063af3040f0e09bdc0a2a860594bf31
Atapi <34801996+Sterophonick@users.noreply.github.com>
2025-05-26 19:15:57 -0600

    test

--- site/blog/friedrice/index.php
+++ site/blog/friedrice/index.php
@@ -17,12 +17,37 @@ Content Warning // Non-Vegan Cooking
 <br />
 <h2>Introduction</h2>
 <p>
+    If you've ever tried chinese-style takeout fried rice at home, but have been disappointed with the results, then I guess this page might be for you. I'm no gourmet chef but I can navigate my way around a recipe.<br/>
+    <img src="/assets/img/gallery/parkercc_chef_atapi.png"><br/>
+    <small>artwork by @ParkerCC</small><br/><br/>
 
+    I don't use any fancy schmancy techniques or equipment or anything, besides a Wok. You can make some good fried rice with many ingredients that can be acquired here in the United States in your average household.
 </p>
 
 <br/>
 
-<h2>My Collection</h2>
+<h2>Ingredients</h2>
+
+<p>
+    Nothing about these ingredients are really measured or anything. In truth, eyeballing it is a great way to go, as fried rice is a very, very flexible dish that was originally intended to use up whatever the hell was in the house.<br/><br/>
+</p>
+
+<div id="ingredients" style="width:50%; margin:auto; text-align: left;">
+    <ul>
+        <li>
+            Rice (duh) - Preferrably some form of white rice, jasmine is what I use.
+        </li>
+        <li>
+            Light Soy Sauce
+        </li>
+        <li>
+            Dark Soy Sauce
+        </li>
+        <li>
+            MSG
+        </li>
+    <ul>
+</div>
 
 <?php
 
