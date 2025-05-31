d717533c455566b007febe4705ac4753cdcaa6b7
Atapi <34801996+Sterophonick@users.noreply.github.com>
2025-05-30 19:33:59 -0600

    crap

--- site/blog/flash2advance/index.php
+++ site/blog/flash2advance/index.php
@@ -49,7 +49,7 @@ Category: Infodump<br/>
 <h3>128Mb EZ-Flash I (x2)</h3>
 <img width="720px" src="/assets/img/blog/flash2advance/collection_bothez1.jpg"><br/><br/>
 <p>
-Two units that are interestingly quite different from each other. The left unit isa later revision, marked "EZ A10" from <code>2004-03</code>. The unit on the left is also apparently broken, I can't seem to get anything to boot off of it, the flash obviously has gone bad. The left unit has flash chips that are marked with <code>E28F32 \ BVP-10 \ 12D43</code>, and the right unit has ones marked <code>E28F640 J3A120/code>, which looking those up reveals they're from Intel's <i>StrataFlash</i> line of flash memory. Both units share the same static RAM chip, an EliteMT
+Two units that are interestingly quite different from each other. The left unit isa later revision, marked "EZ A10" from <code>2004-03</code>. The unit on the left is also apparently broken, I can't seem to get anything to boot off of it, the flash obviously has gone bad. The left unit has flash chips that are marked with <code>E28F32 \ BVP-10 \ 12D43</code>, and the right unit has ones marked <code>E28F640 J3A120</code>, which looking those up reveals they're from Intel's <i>StrataFlash</i> line of flash memory. Both units share the same static RAM chip, an EliteMT
 LP62S2048X-70LLT 256K CMOS SRAM. It's possible that the "BVP-10" chips are cheaper clones of the Intel StrataFlash chips, which seems to have done super super well for longevity. /sarcasm<br/><br/>
 
 <img width="720px" src="/assets/img/blog/flash2advance/collection_bothez1_pcbf.jpg"><br/>
