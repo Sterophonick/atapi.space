4e6c08a9e8018e2968f1f9f59b8ce509e3c99058
Atapi <sterophonick@gmail.com>
2025-07-17 17:34:09 -0600

    rss test4

--- includes/util.php
+++ includes/util.php
@@ -31,7 +31,7 @@ function constructPageHeader($pageTitle) {
 
     $htmlPage .= '<link rel="stylesheet" href="' . whatStyleSheet() . '">';
 
-    if ($rssLink) {
+    if ($rssLink == 1) {
         $htmlPage .= '<link rel="alternate" type=application/rss+xml" title="Atapi\'s Site RSS Feed!!" href="https://atapi.space/rss.xml" />';
     }
 
