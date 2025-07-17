912595491f1a66c6f7f18951ebcca122085d1547
Atapi <sterophonick@gmail.com>
2025-07-17 14:47:46 -0600

    easylink

---
+++ site/blog/easylink/index.php
@@ -0,0 +1,38 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/util.php';
+echo constructPageHeader("Atapi's Domain! :: Blog :: Please Contact Fisher-Price Customer Relations");
+
+?>
+
+<h1><img style="vertical-align:middle" src="/assets/img/blog/icon.png"> Please Contact Fisher-Price Customer Relations</h1>
+<p style="margin-top: -25px;"><br/></p><br/>
+<p>
+July ??, 2025<br/>
+Category: Tech<br/>
+</p>
+<br />
+<h2>What</h2>
+<p>
+    So there was this little toy that I had as a wee one, we're talking like, four years old. It would attach to a computer over Windows XP, and some software would read the state of the device and act as a secure browser for young children to play flash games on.<br/>
+    <img width="720px" src="/assets/img/blog/easylink/easylink.jpg"><br/><br/>
+
+    I recently picked one up, since thinking about it made me wonder how the thing operated, it really cannot be that complex, right?<br/>
+    (I'm right, it's basic baby crap.)<br/><br/>
+    <img width="720px" src="/assets/img/blog/easylink/photo1.jpg"><br/><br/>
+
+    A quick disassembly shows that, yeah, there isn't much going on. There's a CY7C63813-SXC 8-bit microcontroller and a bunch of switches, eight reserved for the character key, and five reserved for keyboard keys.<br/>
+    <img width="360px" src="/assets/img/blog/easylink/photo2.jpg"> <img width="360px" src="/assets/img/blog/easylink/photo3.jpg"><br/><br/>
+
+    bruh
+</p>
+
+<br/>
+
+<h2>Allergy Information</h2>
+
+<?php
+
+echo constructPageFooter();
+
+?>
