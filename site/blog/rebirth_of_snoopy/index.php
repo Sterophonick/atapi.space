57a87d51608936f57ec115d357e3372b6a9f50ba
Atapi <sterophonick@gmail.com>
2025-07-23 16:26:30 -0600

    snoops

---
+++ site/blog/rebirth_of_snoopy/index.php
@@ -0,0 +1,60 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/util.php';
+echo constructPageHeader("Atapi's Domain! :: Blog :: Rebirth of SNOOPY");
+
+?>
+
+<h1><img style="vertical-align:middle" src="/assets/img/blog/icon.png"> Rebirth of SNOOPY</h1>
+<p style="margin-top: -25px;">Dumb and Dumber<br/></p><br/>
+<p>
+??? ??, 2025<br/>
+Category: Infodump<br/>
+<span style="color: yellow">
+Content Warning // Language
+</span>
+</p>
+<br />
+
+<h2>The Disk Image</h2>
+<p>
+    As wee one, my main computer was a Dell Dimension 4550 running Windows XP Service Pack 3. It was a hand-me-down from my dad, who, somewhere between 2010 and 2011, upgraded to a machine running Windows 7. It came with an Intel Pentium 4 SL6S2 operating at 2524 MHz, 1 GB of RAM, a 60 GB hard disk with a 250GB secondary drive, an NVIDIA GeForce 6200, and a SoundBlaster Audigy 2 ZS.<br/>
+    <img width="720px" src="/assets/img/blog/rebirth_of_snoopy/machine.jpg"><br/><br/>
+
+    In 2020, I had to recycle the machine to make some space in my room, however not before I made a complete copy of the surface of the main hard disk, and copied all of the files on the secondary disk. The hope was to maybe, one day, virtualize the machine in a way where everything would be accessible, exactly as it was before recycling. A time capsule of random crap I did as a little babyshit.<br/>
+    <img width="720px" src="/assets/img/blog/rebirth_of_snoopy/copying.jpg"><br/><br/>
+
+    That was, of course, five years ago. Recently, I got bored during a rather uneventful drive home, and had already downloaded the image to my lappy, and so I had the idea to start trying to virtualize the old disk image.<br/><br/>
+
+    Information on this type of thing is scarce, much of it being old and straight-up not being applicable to Linux users. Above all else, you have to just. Fuck around until you find out.<br/><br/>
+</p>
+
+<br/>
+
+<h2>Raw Boot</h2>
+<p>
+    The first thing that I tried was to just, create a new virtual machine and mount the image. In QEMU/KVM, I created the VM and mounted the disk image as IDE. Try a boot and....<br/>
+    <img src="/assets/img/blog/rebirth_of_snoopy/bsod1.png"><br/><br/>
+
+    Well... dammit.<br/><br/>
+
+    Apparently, Windows XP is very finicky about IDE controllers, and if you replace your motherboard, it throws a <code>INACCESSIBLE_BOOT_DEVICE</code> tantrum because it only loads chipset drivers related to the original installation. Problem is, <i>we don't have the original machine</i>.<br/><br/>
+
+    So, to the web forums we go.<br/><br/>
+</p>
+
+<br/>
+
+<h2>MergeIDE</h2>
+<p>
+    At the start of my search, I came across this name over and over:<br/>
+    <img width="720px" src="/assets/img/blog/rebirth_of_snoopy/forum1.png"><br/>
+    <img width="720px" src="/assets/img/blog/rebirth_of_snoopy/forum2.png"><br/>
+    <img width="720px" src="/assets/img/blog/rebirth_of_snoopy/forum3.png"><br/>
+</p>
+
+<?php
+
+echo constructPageFooter();
+
+?>
