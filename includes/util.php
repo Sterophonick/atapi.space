4ce4dbc202af2f2fd930a59d456ca40f8a4bfc8f
Atapi <sterophonick@gmail.com>
2025-08-03 14:46:39 -0600

    test

--- includes/util.php
+++ includes/util.php
@@ -200,6 +200,19 @@ function generateScratchEmbed($projectID, $disableMusic=1) {
     echo $htmlEmbed;
 }
 
+// evil code that i copied from irongeek
+function isTorExitPoint(){
+    if (gethostbyname(ReverseIPOctets($_SERVER['REMOTE_ADDR']).".".$_SERVER['SERVER_PORT'].".".ReverseIPOctets($_SERVER['SERVER_ADDR']).".ip-port.exitlist.torproject.org")=="127.0.0.2") {
+        return true;
+    } else {
+        return false;
+    }
+}
+function ReverseIPOctets($inputip){
+    $ipoc = explode(".",$inputip);
+    return $ipoc[3].".".$ipoc[2].".".$ipoc[1].".".$ipoc[0];
+}
+
 // Content helpers
 // These determine whether or not we want to deliver certain content to various browsers
 // This is for ensuring that pages perform well under legacy or otherwise constrained browsers
