6309b147b4857ca40eb016ce589891ce9976d53c
Atapi <sterophonick@gmail.com>
2025-08-03 14:50:40 -0600

    crap

--- includes/util.php
+++ includes/util.php
@@ -202,15 +202,7 @@ function generateScratchEmbed($projectID, $disableMusic=1) {
 
 // evil code that i copied from irongeek
 function isTorExitPoint(){
-    if (gethostbyname(ReverseIPOctets($_SERVER['REMOTE_ADDR']).".".$_SERVER['SERVER_PORT'].".".ReverseIPOctets($_SERVER['SERVER_ADDR']).".ip-port.exitlist.torproject.org")=="127.0.0.2") {
-        return true;
-    } else {
-        return false;
-    }
-}
-function ReverseIPOctets($inputip){
-    $ipoc = explode(".",$inputip);
-    return $ipoc[3].".".$ipoc[2].".".$ipoc[1].".".$ipoc[0];
+
 }
 
 // Content helpers
