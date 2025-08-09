951fd9016bc525d1dce22adccc8e5958bc885df2
Atapi <sterophonick@gmail.com>
2025-08-08 23:51:29 -0600

    remove extra br

--- site/index.php
+++ site/index.php
@@ -197,7 +197,6 @@ src="/assets/img/buttons/siivagunner.png"></a>
 <a href="https://ne0nbandit.art/"><img src="/assets/img/buttons/ne0nbandit.png"></a>
 </div>
 <br />
-<br/>
 
 <?php
 $adURLs = array(
