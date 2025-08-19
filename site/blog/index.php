d98be1f0b0d5190248842b56e9da0c5aaa17c37c
Atapi <sterophonick@gmail.com>
2025-08-18 18:24:58 -0600

    remove the months, it sucks

--- site/blog/index.php
+++ site/blog/index.php
@@ -14,46 +14,25 @@ echo constructPageHeader("Atapi's Domain! :: Blog", $useRssLink = true);
     </p><br/>
 
 <h2>2020</h1>
-<h3>August</h3>
 <a href="gigatron/">Emulating the Gigatron TTL in MAME</a><br />
 <a href="talesfromscratch/">Tales from the Computery Nonsense folder</a><br />
 <br/>
 
 <h2>2024</h2>
-<h3>October</h3>
 <a href="homepage/">Welcome to my Homepage!</a><br />
 <a href="site_theory/">The Theory Behind This Site</a><br />
 <a href="numbers/">Those Weird Numbers from the Sky</a><br />
 <a href="musicilike/">Music I Think is Rad and Stuff</a><br />
-<br/>
-
-<h3>November</h3>
 <a href="gigaleak/">Personal Observations from the Gigaleaks</a><br />
-<br/>
-
-<h3>December</h3>
 <a href="gender/">On Gender</a><br />
 <a href="2024inreview/">2024 Year in Review, I Guess</a><br />
 <br/>
 
 <h2>2025</h2>
-<h3>January</h3>
 <a href="penguin0125/">The State of Linux Gaming (as of January 2025)</a><br />
-<br/>
-
-<h3>February</h3>
 <a href="winemidi/">Enabling MIDI Support for Legacy WINE Gaming</a><br />
-<br/>
-
-<h3>March</h3>
 <a href="yarg/">Adventures in Rock Band on Linux</a><br />
-<br/>
-
-<h3>May</h3>
 <a href="friedrice/">Femboy Fried Rice</a><br />
-<br/>
-
-<h3>July</h3>
 <img class="pixelArt" style="vertical-align:middle" src="/assets/img/global/new.gif"><a href="easylink/">Please Contact Fisher-Price Customer Relations</a><br />
 <img class="pixelArt" style="vertical-align:middle" src="/assets/img/global/new.gif"><a href="rebirth_of_snoopy/">Rebirth of SNOOPY</a><br />
 <br/>
