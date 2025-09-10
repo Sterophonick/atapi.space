4573bdc2bc8382e82aa1f061587fde4aef381333
Sterophonick <sterophonick@gmail.com>
2025-09-10 10:13:19 -0600

    drop woff1

--- includes/util.php
+++ includes/util.php
@@ -14,7 +14,6 @@ function constructPageHeader($pageTitle, $useRssLink = false) {
     <meta name="darkreader-lock">
 
     <link rel="preload" href="/assets/woff/ZenMaruGothic-Medium.woff2" as="font" type="font/woff2">
-    <link rel="preload" href="/assets/woff/ZenMaruGothic-Medium.woff" as="font" type="font/woff">
 
     EOF;
 
