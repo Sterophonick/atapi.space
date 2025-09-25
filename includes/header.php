59418b4acdc44423382280ec1cba4c18de958ae7
Sterophonick <sterophonick@gmail.com>
2025-09-24 19:51:44 -0600

    lol

--- includes/header.php
+++ includes/header.php
@@ -623,8 +623,8 @@ $musicPaths = array(
                 <td><a href="/site/dumps/"><img src="/assets/img/global/chip.png"> Dumps</a></td>
                 <td><a href="/site/blog/"><img src="/assets/img/global/folder.png"> Blog</a></td>
                 <td><a href="/site/gallery/"><img src="/assets/img/global/gallery.png"> Gallery</a></td>
-                <td><a href="/site/links/"><img src="/assets/img/global/links.png"> Links</a></td>
-                <td><a href="/site/guestbook/"><img src="/assets/img/global/guestbook.png"> Say hi!</a></td>
+                <td><a href="/site/links/"><img src="/assets/img/global/links.png"> Web Links</a></td>
+                <td><a href="/site/guestbook/"><img src="/assets/img/global/guestbook.png"> Guestbook</a></td>
             </tr>
         </table>
     </header>
