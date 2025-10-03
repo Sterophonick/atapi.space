15227f6bf22dd6d91f6551064554267727bb71ec
Sterophonick <sterophonick@gmail.com>
2025-10-02 21:42:25 -0600

    test2

--- index.php
+++ index.php
@@ -129,7 +129,7 @@
                     $returncode = 0;
                     
                     chdir($_SERVER['DOCUMENT_ROOT']);
-                    exec("git log -1 --pretty=format:%cs", $output, $returncode);
+                    exec("git log -1 --pretty=format:%cs 2&>1", $output, $returncode);
                     
                     echo "Last modified: " . $returncode;
                     print_r($output);
