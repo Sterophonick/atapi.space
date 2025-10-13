c8079545ea2bc60637c8da37928bcbea1cd3f6e3
Sterophonick <sterophonick@gmail.com>
2025-10-12 20:45:56 -0600

    pogoshell: add first photos

--- site/blog/pogoshell/index.php
+++ site/blog/pogoshell/index.php
@@ -124,8 +124,9 @@ Then, we build our PogoShell ROM and can switch between the users freely.
 <img loading="lazy" src="/assets/img/blog/pogoshell/pogo5.png" alt="Our new users in the PogoShell settings menu."><br/><br/>
 
 <p>
-When we switch to a user, we will be sent to the page they were last visiting, but you'll notice that the saves are swapped out for those belonging to their respective user. (TAKE PHOTOS OF THIS)
+When we switch to a user, we will be sent to the page they were last visiting, but you'll notice that the saves are swapped out for those belonging to their respective user. Notice the text at the bottom of both screenshots here.
 </p>
+<img loading="lazy" src="/assets/img/blog/pogoshell/photo0.jpg" alt="The single save belonging to Atapi."><img loading="lazy" src="/assets/img/blog/pogoshell/photo1.jpg" alt="The single save belonging to Pikachu."><br/><br/>
 
 <?php
 
