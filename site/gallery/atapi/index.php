2c6b388a34dafc3cca45aaf99104d2a4fd230719
Sterophonick <sterophonick@gmail.com>
2025-11-10 15:51:15 -0700

    stuff

--- site/gallery/atapi/index.php
+++ site/gallery/atapi/index.php
@@ -1,21 +1,88 @@
 <?php
 
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/util.php';
-echo constructPageHeader("Atapi's Domain! :: Who is Atapi?");
+echo constructPageHeader("Atapi's Domain! :: Gallery :: Refsheet / Info");
 
 ?>
 
-<h1>Who is Atapi?</h1>
-<p>atapi is little anthropormorphic cat, my fursona</p>
-<p>name comes from ATA Packet Interface</p>
-<p>he doesnt have much lore because he is supposed to represent me</p>
-<p>he is a white cat, a little bit of floof, with red and blue stripes on his ears, torso, legs, and tail
+<h1>Refsheet / Info</h1>
+<p>Interesting lore behind the cat you see on your screen.</p><br/>
+
+<h2 id="p1">Bio</h2>
+<p>
+Atapi (he/she/they) is an anthropomorphic shorthaired domestic cat, who appears as a self-insert character to my real-life self. He doesn't really have any dedicated lore of his own, as he is supposed to be just "me if I was a fuzzy cat."<br/><br/>
+
+His name originates from <a href="https://en.wikipedia.org/wiki/ATAPI"><i>ATA Packet Interface</i></a>, which was a legacy standard used for sending SCSI commands to removable storage mediums such as CD-ROM, DVD, or LTO Tapes, primarily made for IBM PC-compatible machines. It's since fizzled out of use in favor of the much faster Serial ATA protocol, and much more recently, NVMe.<br/><br/>
+
+He was originally created from a doodle I made one evening in November of 2020 because it was still the middle of lockdown and I had nothing to really do. There wasn't really much intent or thought in regards to the drawing, it just kinda came together as my hand led the way to drawing the little critter. Of course, because I'm me, the drawing was also done in <i>Mario Artist: Paint Studio</i> for the Nintendo 64DD.<br/>
+<img width="512px" loading="lazy" src="/assets/img/gallery/atapi_ogdoodle.png"><br/><br/>
+
+What little early (pre-June 2021) artwork exists depicts him having these odd Pikachu-like ears, in large part because I had no clue how to draw ears at the time and couldn't commission any artists to make anything. Sometimes it still happens if it fits the current bit.</p>
+
+<table loading="lazy" class="galleryTable" width="100%" align="center" border="0" cellspacing="10" cellpadding="0" style="text-align: center; font-size: 13pt;">
+    <tr>
+        <td>
+            <img loading="lazy" class="galleryImg" src="/assets/img/gallery/scottallrubin_atapi1.png">
+            <p>March 2021 - AllScottRubin<br/>Before I determined he was a cat.</p>
+        </td>
+        <td>
+            <img loading="lazy" class="galleryImg" src="/assets/img/gallery/atapi_youlikedontyou.png">
+            <p>May 2023 - Myself<br/>A riff on the boykisser meme by Mauzymice,<br/>which also lacked any particular detail in the ears.</p>
+        </td>
+  </tr>
+</table><br/>
+
+<p>
+As for why a cat? It's pretty simple, really. I like cats, and have always loved them ever since I was a wee one.<br/><br/>
+
+Meet Cleo, she was a shorthaired Tabby that I had as a really young kid, up until her passing in 2011. She's a cat that would snuggle up to me in my bed, helping protect me from the monsters in the closet or the people who were out to get me like in the crime shows I saw my parents watching.</br>
+<img width="512px" loading="lazy" src="/assets/img/gallery/bio_ref/cleo.jpg"><br/><br/>
+
+I think she's the main reason I am such a cat lover, as opposed to the other cat we had who at the time was kind of mean. In retrospect, I can't say I blame her, I was a loud small child running around her once peaceful home. 
+</p><br/>
+
+<h2 id="p2">Refsheet / No-No's</h2>
+<p>
+    I don't really have an idea for a dedicated reference sheet image, especially one this detailed, but I will go over some important details and no-no's, think like David Silverman's sheets for The Simpsons. A dedicated web page is easier to edit anyway.<br/><br/>
+    
+    I'm not particularly strict with anything defined here unless explicitly stated otherwise. Feel free to bend the rules if it suits your specific art style.
+</p><br/>
+
+<h3 id="p2_1">Clothing</h3>
+<p>
+    If you look in the gallery, you'll notice that Atapi almost exclusively wears an unzipped reddish/bergundy-colored hoodie. It should be noted that the outer pockets and hood are colored a neutral grey color. There are also pockets on the inside of the hoodie, in the same positions as the outer pockets. draw those as the same color, relative to lighting. If you choose to add drawstrings, make them white with grey aglets.<br/>
+    <img width="512px" loading="lazy" src="/assets/img/gallery/bio_ref/jacket.png"><br/><br/>
+    
+    Most often, he will be wearing blue denim shorts. In general, I prefer if these shorts were a somewhat darker color and were more figure-hugging, like these womens bermuda shorts pictured here. Hemline positions can vary depending on the outfit that you are drawing, and I'm a fan of the lighter-colored cuff at the bottom, it looks cool. <br/>
+    <img loading="lazy" src="/assets/img/gallery/bio_ref/shorts.jpg"><br/><br/>
+     
+    More feminine outfits can depict him wearing skirts. There's a pet peeve of mine and it's when the tail is drawn lifting the skirt upward. The tail is largely optional, but if you draw the tail, do not do this! (And besides, furgonomics are fun, get creative!)
 </p>
-<p>he can be usually seen wearing a blue hat with a 6502 chip on it, a red sweatshirt with the zipper
-undone, and blue shorts</p>
-<p>most of the art in the gallery has pikachu-like ears but those were made before the design was finalized
+
+<table loading="lazy" class="galleryTable" width="100%" align="center" border="0" cellspacing="10" cellpadding="0" style="text-align: center; font-size: 13pt;">
+    <tr>
+        <td>
+            <img loading="lazy" class="galleryImg" src="/assets/img/gallery/bio_ref/skirt_no.png">
+            <p>No!</p>
+        </td>
+        <td>
+            <img loading="lazy" class="galleryImg" src="/assets/img/gallery/bio_ref/skirt_yes.png">
+            <p>Yes!</p>
+        </td>
+  </tr>
+</table><br/>
+
+<p>
+    Some of the drawings have him wearing socks, and I think I would prefer primarily black knee-high socks with the triple striped top, colors can vary.. If you also draw shoes, do black Converse Chucks with white soles, lace colors can vary.<br/>
+    <img loading="lazy" src="/assets/img/gallery/bio_ref/socks.png"><br/><br/>
+    
+    These days, Atapi is often depicted wearing a black T-shirt. Designs can vary, get creative and go silly on it.<br/><br/>
+    
+    Early drawings of his blue ballcap often show a DIP 6502 on it, but I pretty much have since dropped that. If you want a design on the hat, be my guest, but I don't really have one much anymore. Alternate outfits can change this color, but by default it is blue.<br/><br/>
+    
+    
 </p>
-<p>rest assured, the ears are cat ears</p>
+
 <br />
 
 <?php echo constructPageFooter(); ?>
