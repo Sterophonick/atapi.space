5c365364cfb6f1fe9320fcfce894c4ca8101b1ef
Sterophonick <sterophonick@gmail.com>
2026-01-08 11:44:46 -0700

    fixup addiction pitchbends

--- includes/util.php
+++ includes/util.php
@@ -28,7 +28,7 @@ function constructPageHeader($pageTitle, $useRssLink = false) {
     }
 
     $htmlPage .= <<<EOF
-    <link rel="shortcut icon" href="/assets/img/global/favicon.ico">
+    <link rel="shortcut icon" href="/assets/img/global/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8" />
     </head>
