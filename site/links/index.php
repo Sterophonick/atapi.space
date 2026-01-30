b8ed99977e4b48c7cf8187e6e86b2cb555e573bd
Sterophonick <sterophonick@gmail.com>
2026-01-29 22:48:52 -0700

    woops

--- site/links/index.php
+++ site/links/index.php
@@ -96,7 +96,7 @@ Here's some links to cool netizens!<br/>
 <a href="https://aceraccoon.neocities.org/"><img src="/assets/img/buttons/aceracc.gif"></a>
 <a href="https://ne0nbandit.art/"><img src="/assets/img/buttons/ne0nbandit.png"></a>
 <a href="https://swiftyshq.neocities.org/"><img src="/assets/img/buttons/swiftyshq.png"></a>
-<a href="https://genesisjam.neocities.org/"><img src="/assets/img/buttons/genesisjam.gif"></a>
+<a href="https://www.genesisjam.net/"><img src="/assets/img/buttons/genesisjam.gif"></a>
 </div>
 
 <br/>
