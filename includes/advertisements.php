1775a3a0a122e21ed0f19e578184aef17ab23453
Sterophonick <sterophonick@gmail.com>
2026-03-26 18:21:19 -0600

    lol

--- includes/advertisements.php
+++ includes/advertisements.php
@@ -290,6 +290,7 @@ $adList = array(
     // absolute bullshit
     "/assets/img/ads/KDE_plasma.gif","https://kde.org/plasma-desktop/",
     "/assets/img/ads/devkitpro.gif","https://devkitpro.org/",
+    "/assets/img/ads/creeper-apocalypse.png","https://web.archive.org/web/20120228034613im_/http://samlancashire.com/quiz/creeper/",
 
     // made by me!
     "/assets/img/ads/java-funny.gif","https://www.youtube.com/watch?v=jwJvj7BV41g", // idea came from friend jae
