8c2973f04859a0909c296542f1bd90cd0eb008dc
Sterophonick <sterophonick@gmail.com>
2026-04-18 13:56:52 -0600

    Crap

--- site/gallery/index.php
+++ site/gallery/index.php
@@ -445,16 +445,11 @@ echo constructPageHeader("Atapi's Domain! :: Gallery");
         <img loading="lazy" class="galleryImg galleryGlow" src="/assets/img/gallery/get_it_white_boy.png"></a>
         <p>December 2025<br />oh yeah break it down white boy<br/>(traced from noodlefunny)<br /><a href="/files/get_it_white_boy.xcf">Source File</a><br /></p>
     </td>
-    <td>
-        <img loading="lazy" class="galleryImg" src="/assets/img/gallery/cabbit.png"></a>
-        <p>January 2026<br />cabbit<br /><a href="/files/cabbit.xcf">Source File</a><br /></p>
-    </td>
     
     <td>
         <img loading="lazy" class="galleryImg" src="/assets/img/gallery/cabbit.png"></a>
         <p>January 2026<br />cabbit<br /><a href="/files/cabbit.xcf">Source File</a><br /></p>
     </td>
-    
     <td>
         <img loading="lazy" class="galleryImg galleryGlow" src="/assets/img/gallery/drpepper.png"></a>
         <p>April 2026<br />here have royal crown !!!<br /><a href="/files/drpepper.xcf">Source File</a><br /></p>
