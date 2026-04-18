4372ff205380d7d01c6fbd6cf88c01bb85dc3ad4
Sterophonick <sterophonick@gmail.com>
2026-04-18 12:07:49 -0600

    what

--- includes/lastfm.php
+++ includes/lastfm.php
@@ -2,7 +2,7 @@
 
 // Call the LastFM REST API to get the stats of my shit because I can't trust many other services to do exactly what I want.
 
-$lastFmKey = @trim(file_get_contents("/srv/.lfm_key")); // Do not store this in my source code. Always use a way around it like an env var or systemd edit.
+$lastFmKey = trim(file_get_contents("/srv/.lfm_key")); // Do not store this in my source code. Always use a way around it like an env var or systemd edit.
 $lastFmUser = "Sterophonick"; // Of course, this can be what you want, but since this is my world, we use my user.
 
 // This will return an array with four things:
