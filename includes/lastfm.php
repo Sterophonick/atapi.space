9137c7b6b620a68704dff497435514be0ba1e592
Sterophonick <sterophonick@gmail.com>
2026-04-18 12:14:18 -0600

    what2

--- includes/lastfm.php
+++ includes/lastfm.php
@@ -1,8 +1,8 @@
 <?php
 
-// Call the LastFM REST API to get the stats of my shit because I can't trust many other services to do exactly what I want.
+// Call the LastFM REST API to get the stats of my LastFM acc because I can't trust many other services to do exactly what I want.
 
-$lastFmKey = trim(file_get_contents("/srv/.lfm_key")); // Do not store this in my source code. Always use a way around it like an env var or systemd edit.
+$lastFmKey = @trim(file_get_contents("/srv/.lfm_key")); // Do not store this in my source code. Always use a way around it like an env var or systemd edit.
 $lastFmUser = "Sterophonick"; // Of course, this can be what you want, but since this is my world, we use my user.
 
 // This will return an array with four things:
@@ -15,7 +15,7 @@ function getLastFmData() {
     global $lastFmUser;
     global $lastFmKey;
     
-    $lastFmDataArray = array("", "", "[Unknown Album]", "/assets/img/home/lastfmicons/empty.png", "");
+    $lastFmDataArray = array("Failed to get LastFM data.", "", "", "/assets/img/home/lastfmicons/empty.png", "https://www.last.fm/user/Sterophonick");
     
     $lastFmFetch = @file_get_contents("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" . $lastFmUser . "&api_key=" . $lastFmKey . "&format=json");
     
@@ -35,7 +35,8 @@ function getLastFmData() {
     $imgUrl = $lastFmDecode["recenttracks"]["track"][0]["image"][0]["#text"];
     if(!empty($imgUrl)) $lastFmDataArray[3] = $imgUrl;
     
-    $lastFmDataArray[4] = $lastFmDecode["recenttracks"]["track"][0]["url"];
+    $trackUrl = $lastFmDecode["recenttracks"]["track"][0]["url"];
+    if(!empty($trackUrl)) $lastFmDataArray[4] = $trackUrl;
     
     return $lastFmDataArray;
 }
